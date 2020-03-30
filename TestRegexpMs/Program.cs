efef76f6cbfe1b53abe4361f9d1ccb5b83ce26b9
rummolprod999 <rummolprod999@gmail.com>
2020-03-30 08:02:28 +0300

    Initial commit

---
+++ TestRegexpMs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestRegexpMs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
